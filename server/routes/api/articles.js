9bd73f0defcf3e1b2cf39bf29edf62e0841eb80a
James <jwinham4740@gmail.com>
2018-02-23 15:24:08 -0500

    asfsf

--- server/routes/api/articles.js
+++ server/routes/api/articles.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const articleController = require("../../controllers/articleController");
+const articleController = require("../../controllers/articlecontroller");
 
 // Matches with "/api/articles"
 router.route("/")
